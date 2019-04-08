75ffaefcb0566d2e38ec945b7b75680ce6ebe475
Siev Hengly <henglysiev4433@gmail.com>
2019-04-09 01:12:41 +0700

    Database Schema for blog posts

--- resources/views/_includes/nav/manage.blade.php
+++ resources/views/_includes/nav/manage.blade.php
@@ -4,7 +4,21 @@
       General
     </p>
     <ul class="menu-list">
-      <li><a href="{{route('manage.dashboard')}}" class="{{ Nav::isRoute('manage.dashboard') }}">Dashboard</a></li>
+      <li><a href="{{route('users.index')}}" class="{{ Nav::isResource('posts', 2) }}">Dashboard</a></li>
+    </ul>
+
+    <p class="menu-label">
+      Content
+    </p>
+    <ul class="menu-list">
+      <li><a href="{{ route('posts.index') }}" class="{{ Nav::isResource('users') }}">Blog Posts</a></li>
+      <!-- <li>
+        <a class="has-submenu {{ Nav::hasSegment(['roles', 'permissions'], 2) }}">Roles &amp; Permissions</a>
+        <ul class="submenu">
+          <li><a href="{{ route('roles.index') }}">Roles</a></li>
+          <li><a href="{{ route('permissions.index') }}">Permissions</a></li>
+        </ul>
+      </li> -->
     </ul>
 
     <p class="menu-label">
